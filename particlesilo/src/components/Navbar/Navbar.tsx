2242798ce51fbfb2b5ae1a1a05b8faada1442d27
Manya Agarwal <magarwal@igalia.com>
2023-03-10 17:02:51 +0800

    add GitHub authentication

--- particlesilo/src/components/Navbar/Navbar.tsx
+++ particlesilo/src/components/Navbar/Navbar.tsx
@@ -1,3 +1,4 @@
+import { useSession } from "next-auth/react";
 import Button from "../Button"
 import NavbarItem from "./NavbarItem"
 
@@ -8,31 +9,40 @@ enum NavbarItemType {
 }
 
 export default function Navbar(){
+
+    const { data: session, status } = useSession();
+   
     const items = [
         {
             title: "Resources", 
             route: "/resources", 
-            type: NavbarItemType.text
+            type: NavbarItemType.text, 
+            hide: !session
         }, 
         {
             title: "Upload", 
             route: "/upload",
-            type: NavbarItemType.text
+            type: NavbarItemType.text, 
+            hide: !session
         }, 
         {
-            title: "Login", 
-            route: "/login", 
-            type: NavbarItemType.borderButton
+            title: "Sign In", 
+            route: "/api/auth/signin", 
+            type: NavbarItemType.borderButton, 
+            hide: !!session
         }, 
         { 
             title: "Sign Up", 
-            route: "/signup", 
-            type: NavbarItemType.fillButton
+            route: "/api/auth/signup", 
+            type: NavbarItemType.fillButton, 
+            hide: !!session
         }
     ]
+
     return <div className="min-w-screen h-24 flex space-x-8 justify-end ">
         {
-            items?.map(item => item.type === "TEXT" ? <NavbarItem title={item.title} route={item.route} key={item.title} /> : <Button title={item.title} link={item.route} fill={item.type === "FILLBUTTON"} key={item.title} />)
+            items?.filter(i => !i.hide).map(item => 
+                item.type === "TEXT" ? <NavbarItem title={item.title} route={item.route} key={item.title} /> : <Button title={item.title} link={item.route} fill={item.type === "FILLBUTTON"} key={item.title} />)
         }
     </div>
 }
